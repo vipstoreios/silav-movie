2111bc9136d8d5065d3423b350c3ee0c9c5e5297
IOS Proj <78811732+vipstoreios@users.noreply.github.com>
2026-09-09 02:57:20 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,14 @@
 export default function Navbar() {
   return (
-    <nav>
-      <h1>Silav Movie 🎬</h1>
+    <nav className="container flex justify-between items-center py-6">
+      <h1 className="text-3xl font-bold text-red-600">
+        Silav Movie 🎬
+      </h1>
 
-      <div>
-        <a href="/">Home</a>
-        <a href="/movies">Movies</a>
-        <a href="/series">Series</a>
+      <div className="flex gap-6 text-gray-300">
+        <a href="/">سەرەتا</a>
+        <a href="/movies">فیلم</a>
+        <a href="/series">دراما</a>
       </div>
     </nav>
   );

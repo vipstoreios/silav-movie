da3b3dde34a8d4a233ea18a1cb009b48fb34345a
IOS Proj <78811732+vipstoreios@users.noreply.github.com>
2026-09-09 02:48:26 +0300

    Create Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Silav Movie 🎬</h1>
+
+      <div>
+        <a href="/">Home</a>
+        <a href="/movies">Movies</a>
+        <a href="/series">Series</a>
+      </div>
+    </nav>
+  );
+}

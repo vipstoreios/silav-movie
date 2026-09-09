160cb2f8476ae23c23d16fa15e2c8b2809ee32a2
IOS Proj <78811732+vipstoreios@users.noreply.github.com>
2026-09-10 00:10:13 +0300

    Fix static robots for GitHub Pages

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,11 @@
 import type { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: '*', allow: '/', disallow: ['/admin/'] }],
-    sitemap: 'https://silav-movie.vercel.app/sitemap.xml',
+    sitemap: 'https://vipstoreios.github.io/silav-movie/sitemap.xml',
   };
 }

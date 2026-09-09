c3e856330201dbef8883690ccfd1c9cb6c2171a4
IOS Proj <78811732+vipstoreios@users.noreply.github.com>
2026-09-09 03:13:52 +0300

    Update Category.tsx

--- components/Category.tsx
+++ components/Category.tsx
@@ -3,30 +3,41 @@ const categories = [
   "دراما",
   "ئەکشن",
   "کۆمیدی",
-  "ترسناک",
-  "ئەنیمەیشن",
+  "ترسناک"
 ];
 
 
 export default function Category() {
+
   return (
-    <div className="container flex gap-4 overflow-x-auto py-4">
+
+    <div className="
+      container
+      flex
+      gap-4
+      overflow-x-auto
+    ">
 
       {categories.map((item)=>(
+
         <button
           key={item}
           className="
-          bg-zinc-900
-          px-6
-          py-2
-          rounded-full
-          hover:bg-red-600
+            bg-zinc-900
+            px-6
+            py-3
+            rounded-full
+            hover:bg-red-600
           "
         >
+
           {item}
+
         </button>
+
       ))}
 
     </div>
+
   );
 }

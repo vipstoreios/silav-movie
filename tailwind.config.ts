170b4645f6f3f9cd6d3c574aab9a52eb3fdc1064
IOS Proj <78811732+vipstoreios@users.noreply.github.com>
2026-09-09 03:15:29 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+content:[
+"./app/**/*.{ts,tsx}",
+"./components/**/*.{ts,tsx}"
+],
+theme:{
+extend:{}
+},
+plugins:[]
 };
 
 export default config;

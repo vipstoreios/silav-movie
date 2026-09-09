bfe978b5f9545f1aa153b9d196933f53dc787396
IOS Proj <78811732+vipstoreios@users.noreply.github.com>
2026-09-09 03:13:38 +0300

    Update SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,21 +1,24 @@
 export default function SearchBar() {
+
   return (
-    <div className="container py-6">
+
+    <div className="container">
+
       <input
         type="text"
         placeholder="گەڕان بۆ فیلم..."
         className="
-        w-full
-        bg-zinc-900
-        border
-        border-zinc-700
-        rounded-full
-        px-6
-        py-4
-        text-white
-        outline-none
+          w-full
+          rounded-full
+          bg-zinc-900
+          border
+          border-zinc-700
+          px-6
+          py-4
         "
       />
+
     </div>
+
   );
 }

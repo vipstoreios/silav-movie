fd8a6ce550ede9a02a0a1b5dd170d7c7e3a97834
IOS Proj <78811732+vipstoreios@users.noreply.github.com>
2026-09-09 03:14:09 +0300

    Update MovieCard.tsx

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,35 +1,56 @@
-type MovieCardProps = {
+type Props = {
   title: string;
   year: string;
   image: string;
 };
 
+
 export default function MovieCard({
   title,
   year,
-  image,
-}: MovieCardProps) {
+  image
+}: Props) {
+
+
   return (
-    <div className="bg-zinc-900 rounded-xl overflow-hidden shadow-lg">
+
+    <div className="
+      bg-zinc-900
+      rounded-xl
+      overflow-hidden
+    ">
+
       <img
         src={image}
         alt={title}
-        className="w-full h-80 object-cover"
+        className="
+          w-full
+          h-80
+          object-cover
+        "
       />
 
+
       <div className="p-4">
+
         <h2 className="text-xl font-bold">
           {title}
         </h2>
+
 
         <p className="text-gray-400">
           {year}
         </p>
 
+
         <span className="text-red-500">
           ژێرنوسی کوردی
         </span>
+
       </div>
+
+
     </div>
+
   );
 }

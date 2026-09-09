087cbdf92cdcba6fa208d09c03c4fa2ffbdc263a
IOS Proj <78811732+vipstoreios@users.noreply.github.com>
2026-09-09 23:32:21 +0300

    Assemble upgraded home experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
+import FeatureStrip from '@/components/FeatureStrip';
 import MovieGrid from '@/components/MovieGrid';
-import SearchBox from '@/components/SearchBox';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Navbar />
       <Hero />
-      <SearchBox />
+      <FeatureStrip />
       <MovieGrid />
+      <Footer />
     </main>
   );
 }

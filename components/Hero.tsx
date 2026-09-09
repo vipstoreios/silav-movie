67c60ed418f2cf40d71c67eabcdc9c73e84179f9
IOS Proj <78811732+vipstoreios@users.noreply.github.com>
2026-09-09 03:13:26 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,22 +1,34 @@
 export default function Hero() {
-  return (
-    <section className="container py-16">
-      <div className="bg-gradient-to-r from-red-900 to-black rounded-3xl p-10">
 
-        <h1 className="text-5xl font-bold mb-5">
+  return (
+
+    <section className="container py-10">
+
+      <div className="
+        rounded-3xl
+        p-12
+        bg-gradient-to-r
+        from-red-900
+        to-black
+      ">
+
+        <h1 className="
+          text-5xl
+          font-bold
+          mb-5
+        ">
           بەخێربێیت بۆ Silav Movie 🎬
         </h1>
 
-        <p className="text-xl text-gray-300 max-w-xl">
-          سەیری نوێترین فیلم و دراماکان بکە
-          لەگەڵ ژێرنوسی کوردی.
+
+        <p className="text-xl text-gray-300">
+          فیلم و دراما بە ژێرنوسی کوردی
         </p>
 
-        <button className="mt-8 bg-red-600 px-8 py-3 rounded-full">
-          دەستپێکردن
-        </button>
 
       </div>
+
     </section>
+
   );
 }

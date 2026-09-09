0934cb447894a3617ebbc6319942b9ffeb9175a7
IOS Proj <78811732+vipstoreios@users.noreply.github.com>
2026-09-09 04:03:09 +0300

    Add files via upload

---
+++ components/Hero.tsx
@@ -0,0 +1,34 @@
+export default function Hero() {
+
+  return (
+
+    <section className="container py-10">
+
+      <div className="
+        rounded-3xl
+        p-12
+        bg-gradient-to-r
+        from-red-900
+        to-black
+      ">
+
+        <h1 className="
+          text-5xl
+          font-bold
+          mb-5
+        ">
+          بەخێربێیت بۆ Silav Movie 🎬
+        </h1>
+
+
+        <p className="text-xl text-gray-300">
+          فیلم و دراما بە ژێرنوسی کوردی
+        </p>
+
+
+      </div>
+
+    </section>
+
+  );
+}
